7cb99331700ad610f56daa7fb7429f9704516aba
selgyun <kindkiz@naver.com>
2024-07-30 17:18:14 +0900

    feat: 비용 위젯 더미 작업

--- lib/views/widgets/expense_item.dart
+++ lib/views/widgets/expense_item.dart
@@ -12,13 +12,65 @@ class ExpenseItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(1.0),
-      child: Container(
-        color: const Color(0xffFFFFF1),
-        height: 60,
-        child: Center(
-          child: Text(
-            'Item $description',
-            style: const TextStyle(color: Colors.black),
+      child: InkWell(
+        // todo 비용 아이템 토글
+        onTap: () => {debugPrint('Expense item tapped')},
+        child: Container(
+          color: const Color(0xffFFFFF1),
+          height: 70,
+          child: Row(
+            children: [
+              // 금액을 낸 멤버 영역
+              SizedBox(
+                width: 60,
+                // todo 멤버 위젯 넣기
+                child: Container(
+                  color: Colors.red,
+                ),
+              ),
+              // 금액 설명 부분
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(5.0),
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    children: [
+                      // 설명
+                      Text(
+                        '밥값 $description',
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      const Spacer(),
+                      // todo 공유 멤버 리스트
+                      const SizedBox(
+                        height: 20,
+                        child: Text(
+                          'a, b, c',
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              // 비용 액수
+              SizedBox(
+                width: 70,
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  color: Colors.blue,
+                  // todo 비용
+                  child: const Text(
+                    '100\$'
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
