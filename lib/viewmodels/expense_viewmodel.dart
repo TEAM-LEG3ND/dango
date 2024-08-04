6d783a3737d74d9204103a5c34ea1c6577edf940
ammerss <amyss0907@gmail.com>
2024-08-04 16:13:57 -0400

    feat: add-item pop-up design

--- lib/viewmodels/expense_viewmodel.dart
+++ lib/viewmodels/expense_viewmodel.dart
@@ -57,26 +57,130 @@ class ExpenseViewModel extends ChangeNotifier {
         builder: (BuildContext context) {
           return Dialog(
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(0),
               ),
               child: SizedBox(
                 width: 400,
                 height: 300,
                 child: Column(
                   children: [
-                    const Text(
-                      '비용 추가하기',
-                      style: TextStyle(
-                        fontSize: 18,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: DropdownButton<String>(
+                          value: 'Option 1',
+                          items: <String>['Option 1', 'Option 2', 'Option 3']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Center(child: Text(value)),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            // Handle dropdown value change
+                          },
+                          isExpanded: true, // Ensure dropdown takes full width
+                        ),
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text(
-                        '닫기',
+                    Expanded(
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: TextField(
+                                textAlign: TextAlign.center,
+                                textAlignVertical: TextAlignVertical.center,
+                                decoration: InputDecoration(
+                                  hintText: '내용', // Placeholder text
+                                  border: InputBorder.none, // No border
+                                  contentPadding: EdgeInsets.all(
+                                      16.0), // Padding inside the text field
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                                height: 16), // Spacing between input fields
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: TextField(
+                                textAlign: TextAlign.center,
+                                textAlignVertical: TextAlignVertical.center,
+                                decoration: InputDecoration(
+                                  prefixText: '\$ ',
+                                  hintText: '0.00', // Placeholder text
+                                  border: InputBorder.none, // No border
+                                  contentPadding: EdgeInsets.all(
+                                      16.0), // Padding inside the text field
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
+                    ),
+                    // Divider above the buttons
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Divider(
+                        height: 1.5,
+                        thickness: 1,
+                        color: Color.fromARGB(255, 0, 0, 0), // Divider color
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Close dialog
+                            },
+                            style: TextButton.styleFrom(
+                              foregroundColor:
+                                  const Color.fromARGB(255, 0, 0, 0),
+                              minimumSize:
+                                  Size(double.infinity, 50), // Full width
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.zero, // No rounded corners
+                              ),
+                            ),
+                            child: const Text('닫기'),
+                          ),
+                        ),
+                        Container(
+                          height:
+                              50, // Ensure the divider takes the full height
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: VerticalDivider(
+                            width: 1, // Divider thickness
+                            color: Colors.black, // Divider color
+                          ),
+                        ),
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {
+                              // Add confirmation action here
+                              Navigator.of(context).pop(); // Close dialog
+                            },
+                            style: TextButton.styleFrom(
+                              foregroundColor:
+                                  const Color.fromARGB(255, 0, 0, 0),
+                              minimumSize:
+                                  Size(double.infinity, 50), // Full width
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.zero, // No rounded corners
+                              ),
+                            ),
+                            child: const Text('저장'),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
