fcdddbd6b323582d02eacf8090bdddad83f1e6c2
selgyun <kindkiz@naver.com>
2024-08-09 11:18:54 +0900

    feat: 멤버 추가 팝업 뷰 분리

--- lib/viewmodels/expense_viewmodel.dart
+++ lib/viewmodels/expense_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:dango/views/widgets/add_expense_dialog.dart';
+import 'package:dango/views/widgets/add_member_dialog.dart';
 import 'package:flutter/material.dart';
 import '../models/models.dart';
 import '../services/database_service.dart';
@@ -9,9 +10,6 @@ class ExpenseViewModel extends ChangeNotifier {
     fetchExpenses();
     fetchMembers();
   }
-
-  // todo 컨트롤러는 뷰 쪽으로 이동 + 멤버 팝업도 옮겨야 함.
-  final TextEditingController _memberNameCtrl = TextEditingController();
 
   List<Expense> _expenses = [];
   List<Expense> get expenses => _expenses;
@@ -50,115 +48,7 @@ class ExpenseViewModel extends ChangeNotifier {
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
-          return Dialog(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0),
-              ),
-              child: SizedBox(
-                width: 400,
-                height: 300,
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Text(
-                                '멤버 추가',
-                                style: TextStyle(
-                                  fontSize: 22,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                                height: 32
-                            ), // Spacing between input fields
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                              child: TextField(
-                                controller: _memberNameCtrl,
-                                textAlign: TextAlign.center,
-                                textAlignVertical: TextAlignVertical.center,
-                                decoration: const InputDecoration(
-                                  hintText: '새로운 멤버', // Placeholder text
-                                  border: InputBorder.none, // No border
-                                  contentPadding: EdgeInsets.all(16.0), // Padding inside the text field
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    // Divider above the buttons
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Divider(
-                        height: 1.5,
-                        thickness: 1,
-                        color: Color.fromARGB(255, 0, 0, 0), // Divider color
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop(); // Close dialog
-                            },
-                            style: TextButton.styleFrom(
-                              foregroundColor:
-                              const Color.fromARGB(255, 0, 0, 0),
-                              minimumSize:
-                              const Size(double.infinity, 50), // Full width
-                              shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                BorderRadius.zero, // No rounded corners
-                              ),
-                            ),
-                            child: const Text('닫기'),
-                          ),
-                        ),
-                        Container(
-                          height:
-                          50, // Ensure the divider takes the full height
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: const VerticalDivider(
-                            width: 1, // Divider thickness
-                            color: Colors.black, // Divider color
-                          ),
-                        ),
-                        Expanded(
-                          child: TextButton(
-                            onPressed: () {
-                              Member? alreadyMember = _databaseService.getMemberByName(_memberNameCtrl.text);
-                              if (alreadyMember == null) { // DB에 이름이 겹치지 않게 함.
-                                _databaseService.addMember(_memberNameCtrl.text);
-                                fetchMembers();
-                              }
-                              Navigator.of(context).pop(); // Close dialog
-                            },
-                            style: TextButton.styleFrom(
-                              foregroundColor:
-                              const Color.fromARGB(255, 0, 0, 0),
-                              minimumSize:
-                              const Size(double.infinity, 50), // Full width
-                              shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                BorderRadius.zero, // No rounded corners
-                              ),
-                            ),
-                            child: const Text('저장'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ));
+          return const AddMemberDialog();
         });
   }
 
@@ -171,17 +61,11 @@ class ExpenseViewModel extends ChangeNotifier {
         });
   }
 
-  Member? getPaidMemberByName(String? name) {
+  Member? getMemberByName(String? name) {
     if (name != null) {
-      String paidMemberName = name;
-      return _databaseService.getMemberByName(paidMemberName);
+      String memberName = name;
+      return _databaseService.getMemberByName(memberName);
     }
     return null;
-  }
-
-  @override
-  void dispose() {
-    _memberNameCtrl.dispose();
-    super.dispose();
   }
 }
--- lib/views/widgets/add_member_dialog.dart
+++ lib/views/widgets/add_member_dialog.dart
@@ -4,34 +4,25 @@ import 'package:provider/provider.dart';
 import '../../models/models.dart';
 import '../../viewmodels/expense_viewmodel.dart';
 
-class AddExpenseDialog extends StatefulWidget {
-  const AddExpenseDialog({super.key});
+class AddMemberDialog extends StatefulWidget {
+  const AddMemberDialog({super.key});
 
   @override
-  State<AddExpenseDialog> createState() => _AddExpenseDialogState();
+  State<AddMemberDialog> createState() => _AddMemberDialogState();
 }
 
-class _AddExpenseDialogState extends State<AddExpenseDialog> {
-  String? selectedMember;
-
-  final TextEditingController _expenseDescriptionCtrl = TextEditingController(text: '님 께서 낸 돈');
-  final TextEditingController _expenseLabelCtrl = TextEditingController(text: '\$0');
+class _AddMemberDialogState extends State<AddMemberDialog> {
+  final TextEditingController _memberNameCtrl = TextEditingController(text: '새로운 멤버');
 
   @override
   void dispose() {
-    _expenseDescriptionCtrl.dispose();
-    _expenseLabelCtrl.dispose();
-
+    _memberNameCtrl.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final viewModel = Provider.of<ExpenseViewModel>(context);
-
-    if (selectedMember == null && viewModel.members.isNotEmpty) {
-      selectedMember = viewModel.members.first.name;
-    }
 
     return Dialog(
         shape: RoundedRectangleBorder(
@@ -42,60 +33,31 @@ class _AddExpenseDialogState extends State<AddExpenseDialog> {
           height: 300,
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: DropdownButton<String>(
-                      value: selectedMember,
-                      items: viewModel.members
-                          .map<DropdownMenuItem<String>>((Member member) {
-                        return DropdownMenuItem<String>(
-                          value: member.name,
-                          child: Center(child: Text(member.name)),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedMember = newValue;
-                        });
-                      },
-                      isExpanded: true, // Ensure dropdown takes full width
-                      hint: const Text(
-                        '멤버를 추가하세요.',
-                      )
-                  ),
-                ),
-              ),
               Expanded(
                 child: Center(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: TextField(
-                          controller: _expenseDescriptionCtrl,
-                          textAlign: TextAlign.center,
-                          textAlignVertical: TextAlignVertical.center,
-                          decoration: const InputDecoration(
-                            hintText: '우리가 쓴 돈', // Placeholder text
-                            border: InputBorder.none, // No border
-                            contentPadding: EdgeInsets.all(16.0), // Padding inside the text field
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          '멤버 추가',
+                          style: TextStyle(
+                            fontSize: 22,
                           ),
                         ),
                       ),
                       const SizedBox(
-                          height: 16
+                          height: 32
                       ), // Spacing between input fields
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 16.0),
                         child: TextField(
-                          controller: _expenseLabelCtrl,
+                          controller: _memberNameCtrl,
                           textAlign: TextAlign.center,
                           textAlignVertical: TextAlignVertical.center,
                           decoration: const InputDecoration(
-                            prefixText: '\$ ',
-                            hintText: '\$0.00', // Placeholder text
+                            hintText: '새로운 멤버', // Placeholder text
                             border: InputBorder.none, // No border
                             contentPadding: EdgeInsets.all(16.0), // Padding inside the text field
                           ),
@@ -135,7 +97,8 @@ class _AddExpenseDialogState extends State<AddExpenseDialog> {
                     ),
                   ),
                   Container(
-                    height: 50, // Ensure the divider takes the full height
+                    height:
+                    50, // Ensure the divider takes the full height
                     padding: const EdgeInsets.only(bottom: 8.0),
                     child: const VerticalDivider(
                       width: 1, // Divider thickness
@@ -145,26 +108,10 @@ class _AddExpenseDialogState extends State<AddExpenseDialog> {
                   Expanded(
                     child: TextButton(
                       onPressed: () {
-                        // 비용 낸 멤버 가져오기
-                        Member? paidMember = viewModel.getMemberByName(selectedMember);
-
-                        if (paidMember == null) {
-                          debugPrint('[AddExpenseDialog] 비용 지불한 멤버가 없습니다.');
+                        Member? alreadyMember = viewModel.getMemberByName(_memberNameCtrl.text);
+                        if (alreadyMember == null) { // DB에 이름이 겹치지 않게 함.
+                          viewModel.addMember(_memberNameCtrl.text);
                         }
-
-                        // 비용 값 처리
-                        double? expenseValue = double.tryParse(_expenseLabelCtrl.text.replaceAll(RegExp(r'[^0-9.]'), ''));
-
-                        if (expenseValue == null) {
-                          debugPrint('[AddExpenseDialog] 비용 값이 적절하지 않습니다.');
-                        }
-
-                        bool isValid = (paidMember != null && expenseValue != null);
-
-                        if (isValid) {
-                          viewModel.addExpense(_expenseDescriptionCtrl.text, expenseValue, paidMember);
-                        }
-
                         Navigator.of(context).pop(); // Close dialog
                       },
                       style: TextButton.styleFrom(
