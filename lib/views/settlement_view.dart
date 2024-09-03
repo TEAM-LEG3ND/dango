486138593fbbdca682aea2b7b80bdeed0914a7c7
selgyun <kindkiz@naver.com>
2024-09-03 15:24:17 +0900

    feat: 정산 아이템 추가

--- lib/views/settlement_view.dart
+++ lib/views/settlement_view.dart
@@ -1,4 +1,5 @@
 import 'package:dango/viewmodels/settlement_viewmodel.dart';
+import 'package:dango/views/widgets/settlement_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:realm/realm.dart';
@@ -21,9 +22,22 @@ class _SettlementViewState extends State<SettlementView> {
           return const Text('Error');
         }
 
-        return Container(
-
-        );
+        return const Column(children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SettlementItem(from: '태균', to: '석우', cost: 100.0),
+                  SettlementItem(from: '연경', to: '태균', cost: 100.0),
+                  SettlementItem(from: '석우', to: '연경', cost: 100.0),
+                ],
+              ),
+            ),
+          ),
+        ]);
       },
     );
   }
